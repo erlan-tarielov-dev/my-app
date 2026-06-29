ecd813c3c00794a15727865e4a78352ee2de9b4b
Yasir <yasirtolonov989@gmail.com>
2026-06-29 21:46:57 +0600

    yasir

---
+++ src/yasir.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const yasir = () => {
+  return (
+    <div style={{ border: '2px solid gray', padding: '20px', borderRadius: '10px'}}>
+      
+      <img src="https://images.unsplash.com/photo-1781795485048-3d98b53632db?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4NXx8fGVufDB8fHx8fA%3D%3D" alt="my dream house" style={{width: '100%', borderRadius: '8px'}} />
+      <h3 style={{marginTop: '10px'}}>My dream house</h3>
+    </div>
+  )
+}
+
+export default yasir
